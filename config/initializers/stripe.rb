0e45c8f2a0e87ac6f652c63acca362c4e0f52367
krishnateja7 <krrishnateja7@gmail.com>
2016-10-26 18:08:34 -0400

    remove turbo links and fix signup bug

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -1,8 +1,8 @@
 Rails.configuration.stripe = {
 
-:publishable_key => ENV['STRIPE_TEST_PUBLISHABLE_KEY'],
+:publishable_key => 'pk_test_MJjhwmQ6YUnVfzowePlAMFmj',
 
-:secret_key => ENV['STRIPE_TEST_SECRET_KEY']
+:secret_key => 'sk_test_ypZOCsdFagTQ5wUjjkfVSST3'
 
 }
 
